d45e9c8f0299956a1f419947295aabc653099f27
Rizki Sunaryo <rizkisunaryo@gmail.com>
2019-12-19 06:48:39 +0700

    Install ESLint with Standard. And move files into src, to be easily managed

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ const jsonParser = bodyParser.json();
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
-const buildSchemas = require('./src/schemas');
+const buildSchemas = require('./schemas');
 
 db.serialize(() => {
     buildSchemas(db);
 
-    const app = require('./src/app')(db);
+    const app = require('./app')(db);
 
     app.listen(port, () => console.log(`App started and listening on port ${port}`));
 });
